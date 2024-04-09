843d85f9ce999dc13d2ebfaeab327f7b55d9a833
juan.sgamo <juan.sgamo@alumnos.upm.es>
2024-04-09 11:06:10 +0200

    Cambio en rama issue#2

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,4 +1,5 @@
 package hello;
+// Comentario de prueba 2
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
